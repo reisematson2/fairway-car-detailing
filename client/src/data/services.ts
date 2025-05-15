4e328f1d5366a817cc0c08cfb3ac35c6ddaa0de3
reisematson2 <reisematson2@gmail.com>
2025-05-15 19:39:24 +0000

    Update image source URL for service in services data

--- client/src/data/services.ts
+++ client/src/data/services.ts
@@ -114,7 +114,7 @@ export const services: ServiceType[] = [
         ]
       }
     },
-    imageSrc: "https://images.unsplash.com/photo-1715621870807-9e587490502e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    imageSrc: "https://images.unsplash.com/photo-1609532269538-57cbf63135c6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   }
 ];
 
